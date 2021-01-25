487068fc6254fb446ab6e33d7f6dbcb011580712
Kye <promasterboy@outlook.com>
2021-01-25 21:49:25 +1100

    cleaning up & launcher has panel now

--- install.bat
+++ install.bat
@@ -1,1 +1,19 @@
-start npm i
+@echo off
+cd %CD%\Neonite
+IF EXIST node_modules (
+color 03
+echo It seems like you already installed the node modules.
+echo If you want to reinstall them for whatever reason delete the %CD%\node_modules folder
+echo[
+echo[
+echo[
+pause
+) ELSE (
+echo[
+echo Installing node_modules in %CD%
+echo[
+npm i
+pause
+)
+
+
--- run.bat
+++ run.bat
@@ -1,1 +1,5 @@
-start npm start
+@echo off
+echo [96mKye's Fork of Neonite! Credit to Kemo (@xkem0x) (https://github.com/NeoniteDev/neonitev2)[0m
+cd %CD%\Neonite
+START "Fortnite Launcher for Pro Swapper" Launcher\FortniteLauncher.exe
+node app
