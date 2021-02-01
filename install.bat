4df5289ae6e518987e540d85f3c60fc1db92453a
Kye <promasterboy@outlook.com>
2021-02-02 07:30:49 +1100

    fixed install.bat works for everyone

--- install.bat
+++ install.bat
@@ -1,19 +1,2 @@
-@echo off
 cd %CD%\Neonite
-IF EXIST node_modules (
-color 03
-echo It seems like you already installed the node modules.
-echo If you want to reinstall them for whatever reason delete the %CD%\node_modules folder
-echo[
-echo[
-echo[
-pause
-) ELSE (
-echo[
-echo Installing node_modules in %CD%
-echo[
-npm i
-pause
-)
-
-
+start npm i
